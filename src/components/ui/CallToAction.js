78b322e8bc4d783b9a383a63f14afbe8be399d40
uriberg <uribe1927@gmail.com>
2020-09-21 17:34:47 +0300

    'before coming back to tel aviv'

--- src/components/ui/CallToAction.js
+++ src/components/ui/CallToAction.js
@@ -4,6 +4,11 @@ import Typography from "@material-ui/core/Typography";
 import {makeStyles, useTheme} from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
 import ButtonArrow from "./ButtonArrow";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import background from '../../assets/background.jpg';
+import mobileBackground from '../../assets/mobileBackground.jpg';
+import revolutionBackground from "../../assets/repeatingBackground.svg";
 
 const useStyles = makeStyles(theme => ({
     learnButton: {
@@ -14,25 +19,53 @@ const useStyles = makeStyles(theme => ({
         [theme.breakpoints.down("sm")]: {
             marginBottom: "2em"
         }
+    },
+    background: {
+        backgroundImage: `url(${background})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        height: "60em",
+        width: "100%",
+        [theme.breakpoints.down("md")]: {
+            backgroundImage: `url(${mobileBackground})`,
+            backgroundAttachment: "inherit"
+        }
+    },
+    estimateButton: {
+        ...theme.typography.estimate,
+        borderRadius: 50,
+        height: 80,
+        width: 205,
+        backgroundColor: theme.palette.common.orange,
+        fontSize: "1.5rem",
+        marginRight: "5em",
+        marginLeft: "2em",
+        [theme.breakpoints.down("sm")]: {
+            marginLeft: 0,
+            marginRight: 0
+        }
     }
 }));
 
 export default function CallToAction() {
     const classes = useStyles();
     const theme = useTheme();
+    const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
     return (
-        <Grid container>
-            <Grid item>
+        <Grid container alignItems="center" className={classes.background} justify={matchesSM ? "center" : "space-between"} direction={matchesSM ? "column" : "row"}>
+            <Grid item style={{marginLeft: matchesSM ? 0 : "5em", textAlign: matchesSM ? "center" : "inherit"}}>
                 <Grid container direction="column">
                     <Grid item>
-                        <Typography variant="h2">
+                        <Typography variant="h2" style={{fontSize: "1.5rem"}}>
                             Simple Software.<br />Revolutionary Results.
                         </Typography>
                         <Typography variant="subtitle2">
                             Take advantage of the 21st Century.
                         </Typography>
-                        <Grid container item>
+                        <Grid container item justify={matchesSM ? "center" : undefined}>
                             <Button variant="outlined" className={classes.learnButton}>
                                 <span style={{marginRight: 5}}>Learn More</span>
                                 <ButtonArrow width={10} height={10} fill={theme.palette.common.blue}/>
@@ -40,6 +73,9 @@ export default function CallToAction() {
                         </Grid>
                     </Grid>
                 </Grid>
+            </Grid>
+            <Grid item>
+                <Button variant="contained" className={classes.estimateButton}>Free Estimate</Button>
             </Grid>
         </Grid>
     );
